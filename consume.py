23ebf820a5104145f0713512b2a1b02c439942a9
krashr-ds <kylierrn@gmail.com>
2021-01-24 22:04:13 -0500

    In progress: placeholder

---
+++ consume.py
@@ -0,0 +1,12 @@
+#
+# K. Rasku RN BSN & Programmer / Analyst
+# My python data science library
+# Initiated 1/4/2020
+#
+# CONSUME.py
+#
+#### API consumption functions
+#### sensor and simulated sensor data consumption
+#### database and file consumption
+#
+
